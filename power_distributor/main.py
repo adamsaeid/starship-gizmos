8adafc94ff3a97583659521e0f4c7c9d31fa05eb
Adam Saeid <adam.saeid@gmail.com>
2026-02-15 13:27:28 +0000

    #2 move calculation logic into PowerDistributor

--- power_distributor/main.py
+++ power_distributor/main.py
@@ -17,47 +17,9 @@ pins = {
     'clock': clockPIN
 }
 
-weapon_indicator = {
-    "power": 4,
-}
-
-engine_indicator = {
-    "power": 4,
-}
-
-sys_indicator = {
-    "power": 4
-}
-
 power_distributor = PowerDistributor(4,4,4,pins)
 
-def increment_power(indicator, other_indicators):
-    indicator["power"] = min(indicator["power"] + 2, 8)
-   
-    for other_indicator in other_indicators:
-        other_indicator["power"] = max(other_indicator["power"] - 1, 0)
-
-def display_power():
-    sys_power = sys_indicator.get('power')
-    eng_power = engine_indicator.get('power')
-    wep_power = weapon_indicator.get('power')
-
-    power_distributor.set_power(sys_power, eng_power, wep_power)
-
-def handle_press(indicator, other_indicators):
-    increment_power(indicator, other_indicators)
-    display_power()
-
-def handle_weapon_press():
-    handle_press(weapon_indicator, [sys_indicator, engine_indicator])
-
-def handle_engine_press():
-    handle_press(engine_indicator, [sys_indicator, weapon_indicator])
-   
-def handle_sys_press():
-    handle_press(sys_indicator, [engine_indicator, weapon_indicator])
-
-weapon_button.when_pressed = handle_weapon_press
-engine_button.when_pressed = handle_engine_press
-sys_button.when_pressed = handle_sys_press
+weapon_button.when_pressed = power_distributor.add_wep_pip
+engine_button.when_pressed = power_distributor.add_eng_pip
+sys_button.when_pressed = power_distributor.add_sys_pip
 
--- power_distributor/power_distributor.py
+++ power_distributor/power_distributor.py
@@ -11,3 +11,30 @@ class PowerDistributor:
         self.wep_power = wep_power
 
         self.power_indicators.set_power(sys_power, eng_power, wep_power)
+
+    # TODO: fix bug when zero pips in a system being drained
+
+    def add_sys_pip(self):
+        print("add sys")
+        new_sys_power = min(self.sys_power + 2, 8)
+        new_eng_power = max(self.eng_power - 1, 0)
+        new_wep_power = max(self.wep_power - 1, 0)
+
+        self.set_power(new_sys_power, new_eng_power, new_wep_power)
+
+    def add_eng_pip(self):
+        print("add eng")
+        new_eng_power = min(self.eng_power + 2, 8)
+        new_sys_power = max(self.sys_power - 1, 0)
+        new_wep_power = max(self.wep_power - 1, 0)
+
+        self.set_power(new_sys_power, new_eng_power, new_wep_power)
+
+
+    def add_wep_pip(self):
+        print("add wep")
+        new_wep_power = min(self.wep_power + 2, 8)
+        new_sys_power = max(self.sys_power - 1, 0)
+        new_eng_power = max(self.eng_power - 1, 0)
+
+        self.set_power(new_sys_power, new_eng_power, new_wep_power)
